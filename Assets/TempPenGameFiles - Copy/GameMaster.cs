d5c15a080e057ce5e0bae300ea7a2e14fb444eed
JJProgrammer72 <32990515+JJProgrammer72@users.noreply.github.com>
2021-01-31 04:53:06 -0800

    Events
    Events

---
+++ Assets/TempPenGameFiles - Copy/GameMaster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public Animator[] animators;
+    public bool GasLeak;
+    public bool TempRising;
+    public bool megumin;
+    public ParticleSystem steam;
+    public ParticleSystem boom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        steam.Stop();
+        boom.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GasLeak)
+            steam.Play();
+        if (megumin)
+            boom.Play();
+
+        if(TempRising)
+		{
+            foreach(Animator ani in animators)
+			{
+                ani.SetTrigger("Open");
+			}
+		}
+
+    }
+}
---
+++ Assets/TempPenGameFiles - Copy/eventCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventCollider : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "TempRising")
+			FindObjectOfType<GameMaster>().TempRising = true;
+
+		if(collision.gameObject.tag == "Gas")
+			FindObjectOfType<GameMaster>().GasLeak = true;
+
+		if (collision.gameObject.tag == "Boom")
+			FindObjectOfType<GameMaster>().megumin = true;
+	}
+}
